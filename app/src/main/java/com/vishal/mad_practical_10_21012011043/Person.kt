054b074037228bd79dad34fe0a3465c29771e5e7
ladav <ladavavishal22@gmail,com>
2023-10-28 18:05:57 +0530

    Initial commit

---
+++ app/src/main/java/com/vishal/mad_practical_10_21012011043/Person.kt
@@ -0,0 +1,27 @@
+package com.vishal.mad_practical_10_21012011043
+
+import android.location.Address
+import org.json.JSONObject
+import java.io.Serializable
+import java.util.jar.Attributes
+
+class Person ( var id:String,
+               var name:String,
+               var emailId:String,
+               var phoneNo:String,
+               var address:String,
+               var latitude:Double,
+               var longitude:Double
+):Serializable{
+    constructor(jsonObject:JSONObject):this("","","","","",0.0,8.0) {
+        id = jsonObject.getString("id")
+        emailId = jsonObject.getString("email")
+        phoneNo = jsonObject.getString("phone")
+        val profileJson = jsonObject.getJSONObject("profile")
+        name = profileJson.getString("name")
+        address = profileJson.getString("address")
+        val locationJson = profileJson.getJSONObject("location")
+        latitude = locationJson.getDouble("lat")
+        longitude = locationJson.getDouble("long")
+    }
+}
